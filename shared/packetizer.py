f2331b0dfb537f2013ae5e83d95020268b3ef579
Holger Kibur <holger.kibur@gmail.com>
2019-05-31 11:07:53 -0700

    Added loading layouts from file

--- shared/packetizer.py
+++ shared/packetizer.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 def bitslice(val, start, stop):
         return (val >> start) & ((1 << (stop - start)) - 1)
@@ -39,6 +40,26 @@ class PacketLayoutContext(object):
         name = self.frame_type_lookup[frame_type]
         return (self.layouts[name], self.layout_byte_lens[name])
 
+    def parse_config_line(self, line):
+        token_list = line.split(" ")[1:]
+        layout = []
+        for i in xrange(0, len(token_list), 2):
+            layout.append((token_list[i], int(token_list[i + 1])))
+        return layout
+
+    def configure_from_file(self, path):
+        self.meta_layout = []
+        self.layouts = {}
+        with open(path, "r") as fd:
+            for line in fd:
+                line = line.strip()
+                if line.startswith("meta"):
+                    self.meta_layout = self.parse_config_line(line)
+                    continue
+                layout_name = line[:line.index(" ")]
+                self.layouts[layout_name] = self.parse_config_line(line)
+        self.resolve_lengths_with_pad()
+
 def get_raw(sock, byte_len):
     buf = bytearray(byte_len)
     window = memoryview(buf)
@@ -74,7 +95,7 @@ def packet_to_dict(raw, layout):
 def pop_packet(sock, layout_context):
     meta_dict = packet_to_dict(get_raw(sock, layout_context.meta_byte_len), layout_context.meta_layout)
     packet_layout, packet_layout_byte_len = layout_context.get_layout_from_frame_type(meta_dict["frame_type"])
-    return packet_to_dict(get_raw(sock, packet_layout_byte_len), packet_layout)
+    return packet_to_dict(get_raw(sock, packet_layout_byte_len), packet_layout), layout_context.frame_type_lookup[meta_dict["frame_type"]]
 
 def serialize_packet_dict(view, packet_dict, layout):
     bit_offset = 0
@@ -105,26 +126,11 @@ def push_packet(sock, content, layout_context, layout_name):
     packet_bytes = bytearray(layout_context.meta_byte_len + layout_context.layout_byte_lens[layout_name])
     main_view = memoryview(packet_bytes)
     meta = {
-        "frame_type": layout_context.frame_type_lookup.index(layout_name)
+        "frame_type": layout_context.frame_type_lookup.index(layout_name),
+        "timestamp": time.time()
     }
     serialize_packet_dict(main_view[:layout_context.meta_byte_len], meta, layout_context.meta_layout) #Slice the main view just to be safe
     serialize_packet_dict(main_view[layout_context.meta_byte_len:], content, layout_context.layouts[layout_name])
     for b in packet_bytes:
         print bin(b)[2:].zfill(8), 
     return packet_bytes
-
-if __name__ == "__main__":
-    lcon = PacketLayoutContext()
-    lcon.layouts["sample"] = []
-    for i in xrange(5):
-        lcon.layouts["sample"].append((str(i), random.randint(1, 20)))
-    lcon.resolve_lengths_with_pad()
-    lcon.frame_type_lookup.append("sample")
-    sample_values = {}
-    for name, width in lcon.layouts["sample"]:
-        sample_values[name] = random.randint(0, (2 ** width) - 1)
-    print sample_values
-    print lcon.layouts["sample"]
-    ser = push_packet(None, sample_values, lcon, "sample")
-    print
-    print packet_to_dict(ser[1:], lcon.layouts["sample"])
